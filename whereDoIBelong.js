ba404e234ae59b38c05d57907773aa9fc975331f
jayant246 <43151324+jayant246@users.noreply.github.com>
2019-07-15 10:30:40 +0530

    Update whereDoIBelong.js

--- whereDoIBelong.js
+++ whereDoIBelong.js
@@ -9,3 +9,11 @@ if(answ == -1) {
 return answ;
  }
 console.log(getIndex(arr, target)) // it will give the index of target element where it should be placed
+
+// -----------------------------------------(another soluton)
+function getIndex(arr, target) {
+arr.push(target)                           // 1st its push the element into array
+arr.sort((a,b) => a > b)		   // then sort the array, so its correct positon will be find out
+return arr.indexOf(target)		   // return the index, which give its accurate positon
+}
+console.log(getIndex(arr, target))
