2a414a288fd89cd7b3cc75ee6ad731f4575c7c09
jayant246 <43151324+jayant246@users.noreply.github.com>
2019-07-16 20:11:15 +0530

    Update diffTwoArrays.js

--- diffTwoArrays.js
+++ diffTwoArrays.js
@@ -14,3 +14,17 @@ function diffArray(arr1, arr2) {
 }
 
 console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]))
+
+// ----------------------------------------------(another solution)
+function diffArray(arr1, arr2) {
+	let newArr = arr1.concat(arr2)
+	let arr = []
+	let answ = newArr.forEach(value => {
+		if(arr1.indexOf(value) == -1 || !arr2.includes(value)) {
+			arr.push(value)
+		}
+	})
+	console.log(arr)
+	return arr
+}
+console.log([1, 2, 3, 5], [1, 2, 3, 4, 5]))
